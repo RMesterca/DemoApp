c990ff074ed03817fb6ce4b844248667c9c3f10a
rmesterca <mesterca@d-tt.nl>
2020-06-13 17:16:26 +0200

    #time 5m create Xcode project and make commit test through XCode source control

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Raluca Mesterca on 13/06/2020.
+//  Copyright © 2020 Fig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
